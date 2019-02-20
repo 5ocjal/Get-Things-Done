4437cbec25fd3f55728d365551326b1a1885f4d1
Tupacz Rafał <Rafal.Tupacz@asseco.pl>
2019-02-20 16:20:09 +0100

    list view fixed - filtering in progress

--- src/app/listview/listview.component.ts
+++ src/app/listview/listview.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Tasks } from '../tasks';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { TasksService } from '../services/tasks.service';
+import { map, takeLast } from 'rxjs/operators';
+import { isNgTemplate } from '@angular/compiler';
 
 
 
@@ -25,86 +27,63 @@ export class ListviewComponent implements OnInit {
     list: '',
   }
 
-  todo: Tasks[] = [];
-  progress: Tasks[] = [];
-  done: Tasks[] = [];
-  cancelled: Tasks[] = [];
-  delete: Tasks[] = [];
-
+  mainList = {
+    todo : [],
+    progress: [],
+    done: [],
+    cancelled: [],
+    delete: []
+  }
 
   ngOnInit() {
     this.taskService.getTasks().subscribe(task => {
 
-      if (this.task.list == 'todo') {
-        this.todo = task;
+      console.log(task);
 
-      } else if (this.task.list == 'progress') {
-        this.progress = task;
-      }
-
-      /*    switch(this.task.list) {
-            case "todo": {
-              this.todo = task;
-              break;
-            }
-            case 'progress': {
-              this.progress = task;
-              break;
-            }
-            case 'done': {
-              this.done = task;
-              break;
-            }
-            case 'cancelled': {
-              this.cancelled = task;
-              break;
-            }
-            case 'delete': {
-              this.delete = task;
-              break;
-            }
-            default: {
-              console.log("Coś poszło nie tak");
-              break;
-            }
-          } */
-
+      task.filter(item => item.list == 'progress')
+      .map( item =>  this.mainList.progress.push(item) )
+       
+      
+      console.log(this.mainList.progress)
     });
+
+    
+    
+}
+
+drop(event: CdkDragDrop<Tasks[]>) {
+  if(event.previousContainer === event.container) {
+  moveItemInArray(
+    event.container.data,
+    event.previousIndex,
+    event.currentIndex,
+  );
+} else {
+  transferArrayItem(
+    event.previousContainer.data,
+    event.container.data,
+    event.previousIndex,
+    event.currentIndex,
+  );
+}
   }
 
-  drop(event: CdkDragDrop<Tasks[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
-  }
+changeModal(item: any) {
+  this.currentTask = item;
+}
 
-  changeModal(item: any) {
-    this.currentTask = item;
-  }
+onSubmit() {
+  if (this.task.title != '' && this.task.list != '') {
+    this.taskService.addItem(this.task);
+    this.task.title = '';
+    this.task.note = '';
 
-  onSubmit() {
-    if (this.task.title != '' && this.task.list != '') {
-      this.taskService.addItem(this.task);
-      this.task.title = '';
-      this.task.note = '';
-
-    }
   }
+}
 
-  clearList(list: string, todo: string) {
-    this.delete.splice(0);
-  }
+clearList(list: string, todo: string) {
+  this.mainList.delete.splice(0);
+}
 
 
 }
