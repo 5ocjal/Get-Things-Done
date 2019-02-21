f18d8ea118d2ec3ebc189577fe81da5689c15995
Tupacz Rafał <Rafal.Tupacz@asseco.pl>
2019-02-21 12:03:32 +0100

    all list are connected to firebase

--- src/app/listview/listview.component.ts
+++ src/app/listview/listview.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Tasks } from '../tasks';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { TasksService } from '../services/tasks.service';
-import { map, takeLast } from 'rxjs/operators';
-import { isNgTemplate } from '@angular/compiler';
-
 
 
 
@@ -28,7 +25,7 @@ export class ListviewComponent implements OnInit {
   }
 
   mainList = {
-    todo : [],
+    todo: [],
     progress: [],
     done: [],
     cancelled: [],
@@ -38,51 +35,51 @@ export class ListviewComponent implements OnInit {
   ngOnInit() {
     this.taskService.getTasks().subscribe(task => {
 
-      console.log(task);
+      task.filter(item => item.list == 'todo').map(item => this.mainList.todo.push(item));
+      task.filter(item => item.list == 'progress').map(item => this.mainList.progress.push(item));
+      task.filter(item => item.list == 'done').map(item => this.mainList.done.push(item));
+      task.filter(item => item.list == 'cancelled').map(item => this.mainList.cancelled.push(item));
+      task.filter(item => item.list == 'delete').map(item => this.mainList.delete.push(item));
 
-      task.filter(item => item.list == 'progress').map( item =>  this.mainList.progress.push(item) )
-       
-      
-      console.log(this.mainList.progress)
     });
 
-    
-    
-}
 
-drop(event: CdkDragDrop<Tasks[]>) {
-  if(event.previousContainer === event.container) {
-  moveItemInArray(
-    event.container.data,
-    event.previousIndex,
-    event.currentIndex,
-  );
-} else {
-  transferArrayItem(
-    event.previousContainer.data,
-    event.container.data,
-    event.previousIndex,
-    event.currentIndex,
-  );
-}
-  }
-
-changeModal(item: any) {
-  this.currentTask = item;
-}
-
-onSubmit() {
-  if (this.task.title != '' && this.task.list != '') {
-    this.taskService.addItem(this.task);
-    this.task.title = '';
-    this.task.note = '';
 
   }
-}
 
-clearList(list: string, todo: string) {
-  this.mainList.delete.splice(0);
-}
+  drop(event: CdkDragDrop<Tasks[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  changeModal(item: any) {
+    this.currentTask = item;
+  }
+
+  onSubmit() {
+    if (this.task.title != '' && this.task.list != '') {
+      this.taskService.addItem(this.task);
+      this.task.title = '';
+      this.task.note = '';
+
+    }
+  }
+
+  clearList(list: string, todo: string) {
+    this.mainList.delete.splice(0);
+  }
 
 
 }
